96ab1426c0c2966522b42986c851903298cb3586
NolanWhiteRoy <nolawhit555@ed.amdsb.ca>
2018-09-24 15:06:34 -0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.happyBirthdayLabel = new System.Windows.Forms.Label();
+            this.confettiGifPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.confettiGifPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // happyBirthdayLabel
+            // 
+            this.happyBirthdayLabel.BackColor = System.Drawing.Color.SteelBlue;
+            this.happyBirthdayLabel.Font = new System.Drawing.Font("NSimSun", 60F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.happyBirthdayLabel.ForeColor = System.Drawing.Color.White;
+            this.happyBirthdayLabel.Location = new System.Drawing.Point(1, 1);
+            this.happyBirthdayLabel.Name = "happyBirthdayLabel";
+            this.happyBirthdayLabel.Size = new System.Drawing.Size(600, 420);
+            this.happyBirthdayLabel.TabIndex = 0;
+            this.happyBirthdayLabel.Text = "Happy Birthday";
+            this.happyBirthdayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.happyBirthdayLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // confettiGifPictureBox
+            // 
+            this.confettiGifPictureBox.Image = global::WindowsFormsApp1.Properties.Resources.imageedit_1_7802775017;
+            this.confettiGifPictureBox.Location = new System.Drawing.Point(-21, -13);
+            this.confettiGifPictureBox.Name = "confettiGifPictureBox";
+            this.confettiGifPictureBox.Size = new System.Drawing.Size(643, 497);
+            this.confettiGifPictureBox.TabIndex = 1;
+            this.confettiGifPictureBox.TabStop = false;
+            this.confettiGifPictureBox.Visible = false;
+            this.confettiGifPictureBox.Click += new System.EventHandler(this.confettiGifPictureBox_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(604, 419);
+            this.Controls.Add(this.confettiGifPictureBox);
+            this.Controls.Add(this.happyBirthdayLabel);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "BirthdayCard";
+            ((System.ComponentModel.ISupportInitialize)(this.confettiGifPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label happyBirthdayLabel;
+        private System.Windows.Forms.PictureBox confettiGifPictureBox;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media;
+using System.Threading;
+
+/// Created By: Nolan White-Roy
+/// Date created: 2018-09-21
+/// Description: A birthday card that meets all criteria listed on the ISC3U assigment post.
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            //Decalre graphics
+            Refresh();
+            Graphics gForm = this.CreateGraphics();
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+            SolidBrush blueBrush = new SolidBrush(Color.SteelBlue);
+            Pen blackPen = new Pen(Color.Black, 2);
+            Pen bluePen = new Pen(Color.SteelBlue, 2);
+            Pen whitePen = new Pen(Color.White, 5);
+            Font timesFont = new Font("TimesNewRoman", 20, FontStyle.Underline);
+
+            //Happy birthday text disappears when clicked
+            happyBirthdayLabel.Visible = false;
+          
+            //Yay Sound plays
+            SoundPlayer Yayplayer = new SoundPlayer(Properties.Resources.Yay);
+            Yayplayer.Play();
+
+            //Confetti Gif is visable for 3 seconds
+            confettiGifPictureBox.Visible = true;
+            confettiGifPictureBox.Refresh();
+            Thread.Sleep(3000);
+            confettiGifPictureBox.Visible = false;
+            confettiGifPictureBox.Refresh();
+           
+            //Message appears with underline
+            gForm.DrawString("I hope your special day is wonderful!", timesFont, whiteBrush, 70, 50);
+
+            //Ballon changes colours
+            gForm.FillEllipse(redBrush, 500, 150, 60, 80);
+            gForm.DrawLine(blackPen, 530, 230, 530, 390);
+            Thread.Sleep(500);
+            gForm.FillEllipse(yellowBrush, 500, 150, 60, 80);
+            Thread.Sleep(500);
+            gForm.FillEllipse(redBrush, 500, 150, 60, 80);
+            Thread.Sleep(500);
+            gForm.FillEllipse(yellowBrush, 500, 150, 60, 80);
+            Thread.Sleep(500);
+            gForm.FillEllipse(redBrush, 500, 150, 60, 80);
+            Thread.Sleep(500);
+            gForm.FillEllipse(yellowBrush, 500, 150, 60, 80);
+            Thread.Sleep(500);
+            gForm.FillEllipse(redBrush, 500, 150, 60, 80);
+            Thread.Sleep(500);
+            gForm.FillEllipse(yellowBrush, 500, 150, 60, 80);
+            Thread.Sleep(500);
+            gForm.FillEllipse(redBrush, 500, 150, 60, 80);
+
+            //Red ballon floats upwwards 
+            gForm.FillEllipse(blueBrush, 500, 150, 60, 80);
+            gForm.DrawLine(bluePen, 530, 230, 530, 390);
+            gForm.FillEllipse(redBrush, 500, 140, 60, 80);
+            gForm.DrawLine(blackPen, 530, 220, 530, 380);
+            Thread.Sleep(100);
+
+            gForm.FillEllipse(blueBrush, 500, 140, 60, 80);
+            gForm.DrawLine(bluePen, 530, 220, 530, 380);
+            gForm.FillEllipse(redBrush, 500, 130, 60, 80);
+            gForm.DrawLine(blackPen, 530, 210, 530, 370);
+            Thread.Sleep(100);
+
+            gForm.FillEllipse(blueBrush, 500, 130, 60, 80);
+            gForm.DrawLine(bluePen, 530, 210, 530, 370);
+            gForm.FillEllipse(redBrush, 500, 120, 60, 80);
+            gForm.DrawLine(blackPen, 530, 200, 530, 360);
+            Thread.Sleep(100);
+
+            gForm.FillEllipse(blueBrush, 500, 120, 60, 80);
+            gForm.DrawLine(bluePen, 530, 210, 530, 360);
+            gForm.FillEllipse(redBrush, 500, 110, 60, 80);
+            gForm.DrawLine(blackPen, 530, 190, 530, 350);
+            Thread.Sleep(100);
+
+            gForm.FillEllipse(blueBrush, 500, 110, 60, 80);
+            gForm.DrawLine(bluePen, 530, 190, 530, 370);
+            gForm.FillEllipse(redBrush, 500, 100, 60, 80);
+            gForm.DrawLine(blackPen, 530, 180, 530, 340);
+            Thread.Sleep(100);
+
+            gForm.FillEllipse(blueBrush, 500, 100, 60, 80);
+            gForm.DrawLine(bluePen, 530, 180, 530, 360);
+            gForm.FillEllipse(redBrush, 500, 90, 60, 80);
+            gForm.DrawLine(blackPen, 530, 170, 530, 330);
+            Thread.Sleep(100);
+
+            gForm.FillEllipse(blueBrush, 500, 90, 60, 80);
+            gForm.DrawLine(bluePen, 530, 170, 530, 350);
+            gForm.FillEllipse(redBrush, 500, 80, 60, 80);
+            gForm.DrawLine(blackPen, 530, 160, 530, 320);
+            Thread.Sleep(100);
+
+            //Ballon disappears when it hits the underline
+            gForm.FillEllipse(blueBrush, 500, 80, 60, 80);
+
+            //Pop sound while ballon disappears
+            SoundPlayer Popplayer = new SoundPlayer(Properties.Resources.Pop);
+            Popplayer.Play();
+           
+            Thread.Sleep(3000);
+          
+            //Run Again Button Appears
+            happyBirthdayLabel.Text = "";
+            happyBirthdayLabel.Visible = true;        
+            happyBirthdayLabel.BackColor = Color.White;
+            happyBirthdayLabel.Width = this.Width / 6;
+            happyBirthdayLabel.Height = this.Height / 15;
+            happyBirthdayLabel.Location = new Point(250, 350);
+            confettiGifPictureBox.Visible = false;
+            gForm.DrawString("I hope your special day is wonderful!", timesFont, whiteBrush, 70, 50);
+
+            happyBirthdayLabel.Refresh();
+
+            Graphics gHappyBirthdayLabel = happyBirthdayLabel.CreateGraphics();
+            Font arialFont = new Font("Arial", 15, FontStyle.Bold);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+
+            gHappyBirthdayLabel.DrawString("Run Again", arialFont, blackBrush, 0, 0);
+        }
+        private void confettiGifPictureBox_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
